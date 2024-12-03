1fd0a1b53c485c16e30c61fdd829be0a6bd8a655
Igor Derevnin <shroom2207@yandex.ru>
2024-12-03 23:05:40 +0300

    Разбил функцию save_photos

--- save_photos.py
+++ save_photos.py
@@ -18,13 +18,22 @@ def save_photos(links_listed: list = None, folder: str = 'images') -> None:
     os.makedirs(folder, exist_ok=True)
     for link in links_listed:
         image_format = get_image_format(link)
-        image_name = str(links_listed.index(link)).zfill(
-            int(math.ceil(math.log10(len(links_listed)))))
+        image_name = generate_image_name(links_listed, link)
         image_path = os.path.join(folder, image_name)
-        with open(f'{image_path}.{image_format}', 'wb') as image:
-            response = requests.get(link, timeout=20)
-            response.raise_for_status()
-            image.write(response.content)
+        save_image(image_path, image_format, link)
+    return
+
+
+def generate_image_name(links_listed: list, link: str) -> str:
+    return str(links_listed.index(link)).zfill(int(math.ceil(math.log10(len(links_listed)))))
+
+
+def save_image(image_path: str, image_format: str, link: str) -> None:
+    with open(f'{image_path}.{image_format}', 'wb') as image:
+        response = requests.get(link, timeout=20)
+        response.raise_for_status()
+        image.write(response.content)
+    return
 
 
 def main():
