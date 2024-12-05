71c2affed1b793a7f5cc587d69e000544ddbbf80
Igor Derevnin <shroom2207@yandex.ru>
2024-12-05 22:39:32 +0300

    Исправление кода + оформление по PEP8
    Подробности в комментариях к коммиту

--- photobot.py
+++ photobot.py
@@ -9,14 +9,21 @@ import telegram
 from get_photos import get_photos, compress_image
 
 
-def send_photo_with_bot(bot_token: str, group_id: str, time_interval_h: int, photo_paths: list) -> None:
+def send_photo_with_bot(
+    bot_token: str,
+    group_id: str,
+    time_interval_h: int,
+    photo_paths: list,
+    number_of_photos: int,
+) -> None:
     sleep_time = time_interval_h*3600
     bot = telegram.Bot(bot_token)
     while photo_paths:
-        for path in photo_paths:
+        for path in photo_paths[:number_of_photos]:
             with open(f'{path}', 'rb') as photo:
                 bot.send_photo(chat_id=group_id, photo=photo)
                 time.sleep(1)
+        random.shuffle(photo_paths)
         time.sleep(sleep_time)
     return
 
@@ -28,18 +35,17 @@ def main():
     time_interval_h = int(os.getenv('TIME_INTERVAL_H', default='4'))
 
     parser = argparse.ArgumentParser(
-        description='Введите название директории и сколько изображений публиковать'
+        description='Введите название директории '
+        'и сколько изображений публиковать'
     )
     parser.add_argument('-f', '--folder_name', required=True,
                         help='Название директории с изображениями', type=str)
     parser.add_argument('-n', '--number_of_photos',
-                        help='Количество изображений', type=int, default=1)
+                        help='Количество изображений', type=int, default=None)
     args = parser.parse_args()
-    if args.number_of_photos < 1:
-        raise ValueError('Количество изображений должно быть более 1')
 
     folder_name = args.folder_name
-    number_of_photos = args.number_of_photos
+    num_of_photos = args.number_of_photos
 
     photo_paths = get_photos(folder_name=folder_name)
 
@@ -51,25 +57,29 @@ def main():
             compress_image(temp_file_path)
             tempphoto_paths.append(temp_file_path)
 
-        random.shuffle(tempphoto_paths)
+        max_num_of_photos = len(tempphoto_paths)
 
-        if len(tempphoto_paths) < number_of_photos:
-            raise ValueError(
-                f'В папке {folder_name} менее {number_of_photos} изображений'
-                )
-        elif len(tempphoto_paths) > number_of_photos:
-            tempphoto_paths = tempphoto_paths[:number_of_photos]
+        if num_of_photos not in range(1, max_num_of_photos):
+            num_of_photos = max_num_of_photos
 
         try:
-            send_photo_with_bot(bot_token, group_id,
-                                time_interval_h, tempphoto_paths)
+            send_photo_with_bot(bot_token,
+                                group_id,
+                                time_interval_h,
+                                tempphoto_paths,
+                                num_of_photos,
+                                )
         except telegram.error.NetworkError:
             print('Переподключаемся')
             attempt = 0
             while attempt < 5:
                 try:
-                    send_photo_with_bot(bot_token, group_id,
-                                        time_interval_h, tempphoto_paths)
+                    send_photo_with_bot(bot_token,
+                                        group_id,
+                                        time_interval_h,
+                                        tempphoto_paths,
+                                        num_of_photos,
+                                        )
                     break
                 except telegram.error.NetworkError:
                     attempt += 1
@@ -80,8 +90,12 @@ def main():
         except telegram.error.RetryAfter as error:
             print(f'Повтор запроса через {error.retry_after} секунд')
             time.sleep(error.retry_after)
-            send_photo_with_bot(bot_token, group_id,
-                                time_interval_h, tempphoto_paths)
+            send_photo_with_bot(bot_token,
+                                group_id,
+                                time_interval_h,
+                                tempphoto_paths,
+                                num_of_photos,
+                                )
 
 
 if __name__ == '__main__':
